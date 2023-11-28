0aaf6fcecb7aec8373e78e4230b70c1c7ba5a299
nam3isib <111239484+nam3isib@users.noreply.github.com>
2023-11-28 12:58:27 +0100

    new commit

---
+++ medboard/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Navbar from './pages/navbar'
+import {BrowserRouter as Router} from 'react-router-dom'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar/>
+      </div>
+      
+    </Router>
+  )
+}
+
+export default App
