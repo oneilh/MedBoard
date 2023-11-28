e61770d36796ec6672084b95036c183abbb82fe5
nam3isib <111239484+nam3isib@users.noreply.github.com>
2023-11-28 12:58:27 +0100

    new commit

---
+++ medboard/src/pages/Navbar.jsx
@@ -0,0 +1,17 @@
+
+  const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>MEDBOARD</h1>
+            <div className="links">
+              <div><h4>make an appointment</h4></div>
+              <div><h4>write a prescription</h4></div>
+              <div><h4>Generate Report</h4></div>
+            </div>
+        </nav>
+     );
+}
+ 
+
+ 
+export default Navbar;
