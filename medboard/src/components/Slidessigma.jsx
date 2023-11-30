09d5f2b74c31d11d236e9672f7590e224f291f04
nam3isib <111239484+nam3isib@users.noreply.github.com>
2023-11-30 13:05:52 +0100

    new commit

---
+++ medboard/src/components/Slidessigma.jsx
@@ -0,0 +1,33 @@
+const SlidesSigma = () => {
+    return ( 
+        
+        <section className="slidessigma">
+            <div className="doctors">  
+                 <p >Doctors</p>
+                 <p>4567</p>
+            <div className="icon">
+                 </div>
+            </div>
+          
+            <div className="nurse">
+             <p>Nurses</p>
+             <p>4567</p>
+             <div className="icon1"></div></div>
+
+            <div className="patients">
+                <p>Patients</p>
+                
+            <p>4567</p>
+            
+            <div className="icon2"></div></div>
+
+            <div className="pharmacist">
+                <p>Pharmacists</p>
+                <p>4567</p>
+                <div className="icon3"></div></div>
+
+        </section>
+     );
+}
+ 
+export default SlidesSigma;
---
+++ medboard/src/components/content.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const content = () => {
+  return (
+    <div>
+        <div className="ANH">
+
+        </div>
+        <div className="calendar">
+
+        </div>
+        <div className="bookDoc">
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default content
