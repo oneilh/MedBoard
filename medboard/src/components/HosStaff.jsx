6a14e8f18654b1c8f047d643d8244bfae389eed6
nam3isib <111239484+nam3isib@users.noreply.github.com>
2023-12-26 09:09:09 +0100

    newer commit

--- medboard/src/components/HosStaff.jsx
+++ medboard/src/components/HosStaff.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+import doc1 from '../images/doctor-1.jpg'
+import doc2 from '../images/doctor-2.jpg'
+import doc3 from '../images/doctor-3.jpg'
+import doc4 from '../images/doctor-4.jpg'
+
+
+
 
 const HosStaff = () => {
+  
+  const settings =  {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  };
   return (
     <div>
       <div className="staff">
         <h2>HOSPITAL STAFF</h2>
+      </div>
+      <div className="staffimages">
+        <Slider >
+        <div><img src={ doc1 } alt="" /></div>
+        <div><img src={ doc2 } alt="" /></div>
+        <div><img src={ doc3 } alt="" /></div>
+        <div><img src={ doc4 } alt="" /></div>
+        </Slider>
       </div>
     </div>
   )
--- medboard/src/components/Hosbirthanddeath.jsx
+++ medboard/src/components/Hosbirthanddeath.jsx
@@ -1,10 +1,34 @@
+import { Colors } from 'chart.js'
 import React from 'react'
 
 const Hosbirthanddeath = () => {
   return (
    
        <div className="birthanddeath">
-        <h2>HOSPITAL BIRTH AND DEATH ANALYTICS</h2>
+        <h2>HOSPITAL BIRTH & DEATH ANALYTICS</h2>
+        <div className="bda">
+        <div className="birth">
+          Birth <p style={{color :"#007BFF"}}>45.07%</p> <p style={{color :"#007BFF"}}>201,434</p>
+        </div>
+        <div className="death">
+          Death <p style={{color :"#DC3545"}}>29.05%</p> <p style={{color :"#DC3545"}}>134,693</p>
+        </div>
+        <div className="accidents">
+          Accidents <p style={{color :"#FFC107"}}>18.43%</p> <p style={{color :"#FFC107"}}>81,525</p>
+        </div>
+        </div>
+        
+        <div className="progressbar">
+          <div className="blue">
+            Birth
+          </div>
+          <div className="red">
+            death
+          </div>
+          <div className="yellow">
+            Accidents
+          </div>
+        </div>
       </div>
     
   )
